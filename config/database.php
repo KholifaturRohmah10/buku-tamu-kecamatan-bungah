5c563868d608319b6bf5a65f7c678e593e38602b
Kholifatur Rohmah <aremmaaa89@gmail.com>
2026-08-26 16:10:40 +0700

    fix: use dynamic system CA bundle paths for TiDB Serverless SSL

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => [
-                1009 => env('MYSQL_ATTR_SSL_CA') ?: base_path('cacert.pem'),
+                1009 => file_exists('/etc/pki/tls/certs/ca-bundle.crt') ? '/etc/pki/tls/certs/ca-bundle.crt' : (file_exists('/etc/ssl/certs/ca-certificates.crt') ? '/etc/ssl/certs/ca-certificates.crt' : (file_exists('/tmp/cacert_fixed.pem') ? '/tmp/cacert_fixed.pem' : base_path('cacert.pem'))),
                 1014 => true,
             ],
         ],
